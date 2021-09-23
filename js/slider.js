5236adc5785c1f5aa19a7d5520e4363abe674158
KesslerUstra <eduardoksustra@gmail.com>
2021-09-23 15:59:18 -0300

    alterando a pagina participantes

--- js/slider.js
+++ js/slider.js
@@ -1,36 +1,49 @@
 /*----------------Variaveis----------------*/
 
-var slides = document.querySelectorAll('.slides')
+const slider = document.querySelector('.layout-slider');
+const slides = document.querySelectorAll('.slides');
+const buttons = document.querySelectorAll('.button');
+const buttonLeft = document.querySelector('.button-left');
+const buttonRight = document.querySelector('.button-right');
 var imgAtual = 0, imgProx = 1;
-var buttons = document.querySelectorAll('.button')
 var funcao;
-var slider = document.querySelector('.slider')
+
 
 
 /*----------------Funções----------------*/
 
-function rodarImagens(){
+function rodarImagens(direcao){
     slides[imgAtual].classList.remove('slide-visivel');
     slides[imgProx].classList.remove('slide-proximo');
-
     buttons[imgAtual].classList.remove('button-ativo');
 
+    if(direcao == 1){
+        if(imgAtual == (slides.length -1)){
+            imgAtual = 0;
+        }else{
+            imgAtual ++;
+        }
+        if(imgProx == (slides.length -1)){
+            imgProx = 0;
+        }else{
+            imgProx ++;
+        }
+    }else if(direcao == 0){
+        if(imgAtual == 0){
+            imgAtual = slides.length -1;
+        }else{
+            imgAtual --;
+        }
+        if(imgProx == 0){
+            imgProx = slides.length -1;
+        }else{  
+            imgProx --;
+        }
+    }
 
-    if(imgAtual == (slides.length -1)){
-        imgAtual = 0;
-    }else{
-        imgAtual ++;
-    }
-    if(imgProx == (slides.length -1)){
-        imgProx = 0;
-    }else{
-        imgProx ++;
-    }
     slides[imgAtual].classList.add('slide-visivel');
     slides[imgProx].classList.add('slide-proximo');
-
     buttons[imgAtual].classList.add('button-ativo');
-
 }
 
 function parar(){
@@ -38,13 +51,35 @@ function parar(){
 }
 
 function inicio(){
-    funcao = setInterval(rodarImagens,3000);
+    funcao = setInterval(function(){rodarImagens(1)},3000);
+}
+
+function voltar(){
+    rodarImagens(0);
+    buttonPauseAdd(buttonLeft);
+    setTimeout(function(){buttonPauseRemove(buttonLeft)},500);
+}
+
+function prosseguir(){
+    rodarImagens(1);
+    buttonPauseAdd(buttonRight);
+    setTimeout(function(){buttonPauseRemove(buttonRight)},500);
+}
+
+function buttonPauseAdd(button){
+    button.classList.add('button-pause');
+}
+
+function buttonPauseRemove(button){
+    button.classList.remove('button-pause');
 }
 
 function iniciar(){
     inicio()
     slider.addEventListener('mouseenter',parar);
     slider.addEventListener('mouseleave',inicio);
+    buttonLeft.addEventListener('click',voltar);
+    buttonRight.addEventListener('click',prosseguir);
 }
 
 window.addEventListener("load",iniciar);
